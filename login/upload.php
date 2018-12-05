d1dbe92f25da08d951e9d36d2656fb039509d45b
baglan <baglan060495@gmail.com>
2018-12-05 21:33:57 +0500

    newVers

--- login/upload.php
+++ login/upload.php
@@ -1,7 +1,7 @@
 <?php
     $name = $_FILES['img']['name'];
     $tmp_name = $_FILES['img']['tmp_name'];
-	$new_name = "img/upload/$name";
+	$new_name = "../img/upload/$name";
     if(move_uploaded_file($tmp_name, $new_name)){   
 		echo $new_name;
     }else{
